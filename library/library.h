253be3252c6a5dd10e9b435e48338d8ef484e2de
Israel Roldan <israel.alberto.rv@gmail.com>
2020-10-01 00:51:56 -0500

    Refactored the text utilities for clean and convert the escape characters.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- library/library.h
+++ library/library.h
@@ -4,8 +4,8 @@
 #include "html/extract/inner.h"
 #include "html/extract/outer.h"
 #include "utility/text/clean.h"
+#include "utility/text/escape.h"
 #include "utility/text/replace.h"
 #include "utility/text/trim.h"
-#include "utility/text/escape.h"
 
 #endif //AV_TEST_COMPARATOR_LIBRARY_H
