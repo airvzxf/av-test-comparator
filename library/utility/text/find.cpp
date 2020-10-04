e014089fb8835efa93e934ae93e1a5f31f274b38
Israel Roldan <israel.alberto.rv@gmail.com>
2020-10-04 00:51:52 -0500

    Refactored the find utility.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- library/utility/text/find.cpp
+++ library/utility/text/find.cpp
@@ -4,11 +4,9 @@ namespace library::utility::text {
     size_t findFrom(std::string &text, std::string withMatch, size_t startAt, bool inner) {
         bool found;
         size_t position = std::string::npos;
-        char &first = withMatch[0];
 
         for (size_t i = startAt; i < text.length(); ++i) {
-            char &character = text[i];
-            if (character == first) {
+            if (text[i] == withMatch[0]) {
                 found = true;
                 for (int j = 1; j < withMatch.length(); ++j) {
                     if (text[i + j] != withMatch[j]) {
