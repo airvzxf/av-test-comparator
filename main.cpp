b661f04d4be43a1fbc653eb62a61d9b407bfb966
Israel Roldan <israel.alberto.rv@gmail.com>
2020-10-04 03:39:20 -0500

    Added more 12 months for extract and disable the debug display.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,27 +4,22 @@
 int main() {
     auto *settings = core::structure::settings::getInstance();
     settings->error.display = true;
-    settings->debug.display = true;
+    settings->debug.display = false;
 
     const std::string mainUrl = "https://www.av-test.org/en/antivirus/home-windows/windows-10/";
 
     std::vector<std::string> urls;
     urls.emplace_back(mainUrl + "august-2020");
-//    urls.emplace_back(mainUrl + "june-2020");
-//    urls.emplace_back(mainUrl + "april-2020");
-//    urls.emplace_back(mainUrl + "february-2020");
-//    urls.emplace_back(mainUrl + "december-2019");
-//    urls.emplace_back(mainUrl + "october-2019");
-//    urls.emplace_back(mainUrl + "august-2019");
-//    urls.emplace_back(mainUrl + "june-2019");
-//    urls.emplace_back(mainUrl + "april-2019");
-//    urls.emplace_back(mainUrl + "february-2019");
-//    urls.emplace_back(mainUrl + "december-2018");
-//    urls.emplace_back(mainUrl + "october-2018");
-//    urls.emplace_back(mainUrl + "august-2018");
-//    urls.emplace_back(mainUrl + "june-2018");
-//    urls.emplace_back(mainUrl + "april-2018");
-//    urls.emplace_back(mainUrl + "february-2018");
+    urls.emplace_back(mainUrl + "june-2020");
+    urls.emplace_back(mainUrl + "april-2020");
+    urls.emplace_back(mainUrl + "february-2020");
+    urls.emplace_back(mainUrl + "december-2019");
+    urls.emplace_back(mainUrl + "october-2019");
+    urls.emplace_back(mainUrl + "august-2019");
+    urls.emplace_back(mainUrl + "june-2019");
+    urls.emplace_back(mainUrl + "april-2019");
+    urls.emplace_back(mainUrl + "february-2019");
+    urls.emplace_back(mainUrl + "december-2018");
 
     std::vector<core::structure::antivirus> &toCatalog = core::structure::catalog;
     toCatalog.reserve(32);
